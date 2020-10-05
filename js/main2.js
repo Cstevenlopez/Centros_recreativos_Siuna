ab85253710f9d43c80ca1d22795dc517c29313b5
Cstevenlopez <cstevenlopez2001@gmail.com>
2020-10-05 15:47:28 -0600

    Agrege un segundo carrusel

--- js/main2.js
+++ js/main2.js
@@ -1,9 +1,9 @@
-
 const buttonPrev = document.getElementById('button-prev');
 const buttonNext = document.getElementById('button-next');
 const track = document.getElementById('track');
 const slickList = document.getElementById('slick-list');
 const slick = document.querySelectorAll('.slick');
+
 
 const slickWidth = slick[0].offsetWidth;
 
@@ -22,3 +22,33 @@ function Move(value) {
         track.style.left = `${-1 * (leftPosition - slickWidth)}px`;
     }
 }
+
+window.addEventListener('load', function(){
+	new Glider(document.querySelector('.carousel__lista'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: '.carousel__indicadores',
+		arrows: {
+			prev: '.carousel__anterior',
+			next: '.carousel__siguiente'
+		},
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 450,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: 2,
+				slidesToScroll: 2
+			  }
+			},{
+			  // screens greater than >= 1024px
+			  breakpoint: 800,
+			  settings: {
+				slidesToShow: 4,
+				slidesToScroll: 4
+			  }
+			}
+		]
+	});
+});
